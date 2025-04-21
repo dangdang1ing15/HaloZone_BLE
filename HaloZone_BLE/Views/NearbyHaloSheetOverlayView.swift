d99ab0683a3cc70d8d852bb6c1365c29c09d0145
dangdang1ing15 <shine_0512@naver.com>
2025-04-21 11:07:20 +0900

    feat : change sheet view -> overlay view (for animation)

---
+++ HaloZone_BLE/Views/NearbyHaloSheetOverlayView.swift
@@ -0,0 +1,18 @@
+//
+//  NearbyHaloSheetOverlayView.swift
+//  HaloZone_BLE
+//
+//  Created by 성현 on 4/19/25.
+//
+
+import SwiftUI
+
+struct NearbyHaloSheetOverlayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NearbyHaloSheetOverlayView()
+}
